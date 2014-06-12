1bcf0e0d140df9a4482b0503649dbf04252f655f
Antun Krasic <antun@martuna.co>
2014-06-13 01:18:54 +0200

    Refactor Gemfile

--- lib/souschef/gemfile.rb
+++ lib/souschef/gemfile.rb
@@ -1,18 +1,42 @@
 module Souschef
   # Gemfile generator
   class Gemfile < Ruth::Gemfile::Yaml
-    attr_accessor :cwd, :dir
+    attr_accessor :opts
 
     def initialize(opts)
-      @cwd = Dir.pwd
-      @dir = opts[:cookbook]
-      @gemfile = "#{@cwd}/#{@dir}/Gemfile"
+      @opts = opts
+      super(yaml_file_location)
+    end
 
+    # Public - Writes down the Gemfile location
+    #
+    # Returns nil
+    def write
       Souschef::Print.info 'Populating Gemfile'
-      yaml_file = File.expand_path('../../../data/gemfile.yml', __FILE__)
+      write_gemfile
+    end
 
-      super(yaml_file)
-      output
+    private
+
+    # Private - Return location of YAML file
+    #
+    # Returns String
+    def yaml_file_location
+      File.expand_path('../../../data/gemfile.yml', __FILE__)
+    end
+
+    # Private - Return location of Gemfile
+    #
+    # Returns string
+    def gemfile_location
+      File.join(Dir.pwd, @opts[:cookbook], 'Gemfile')
+    end
+
+    # Private - Write Gemfile data
+    #
+    # Returns nil
+    def write_gemfile
+      File.open(gemfile_location, 'w') { |file| file.write(output) }
     end
   end
 end
