364fcf6a5103971e948f457d1319d75336906408
Antun Krasic <antun@martuna.co>
2014-06-13 12:33:27 +0200

    Update comments

--- lib/souschef/print.rb
+++ lib/souschef/print.rb
@@ -1,17 +1,31 @@
 module Souschef
+  # Prit out colorized information
   class Print
-
+    # Public - Print info messate
+    #
+    # msg - String message
+    #
+    # Returns nil
     def self.info(msg)
       puts "~> #{msg}".colorize(:yellow)
     end
 
+    # Public - Print error messate
+    #
+    # msg - String message
+    #
+    # Returns nil
     def self.error(msg)
       puts "#{msg}".colorize(:red)
     end
 
+    # Public - Print warning  messate
+    #
+    # msg - String message
+    #
+    # Returns nil
     def self.warning(msg)
       puts "#{msg}".colorize(:orange)
     end
-
   end
 end
