b6f2079893f4b685a101c18726953d739b243560
Antun Krasic <antun@martuna.co>
2014-10-11 21:20:47 +0200

    Update serverspec helper

--- data/serverspec/serverspec_helper.rb
+++ data/serverspec/serverspec_helper.rb
@@ -1,10 +1,7 @@
 require 'serverspec'
 require 'pathname'
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
+set :backend, :exec
 
 RSpec.configure do |c|
-  c.before :all do
-    c.os = backend(Serverspec::Commands::Base).check_os
-  end
+  c.path = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 end
